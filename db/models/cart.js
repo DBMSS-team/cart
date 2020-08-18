1dc41cf9b9823100c9084de00337b76d9180cf29
Sahil Kumar <sahil.kumar@sap.com>
2020-08-19 00:42:50 +0530

    Update cart.js

--- db/models/cart.js
+++ db/models/cart.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
-const { Decimal128 } = require("bson");
 
-let productSchema = mongoose.Schema({});
-let cartSchema = mongoose.Schema({
-  _id: {
-    type: String,
-  },
-  products: [{ type: String }],
+const Schema = mongoose.Schema;
+
+const cartProductSchema = new Schema({
+  product_id: {type: String, required: true },
+  quantity: {type: Number, required: true },
+});
+
+const cartSchema = new Schema({
+  products: [cartProductSchema],
   store_id: { type: String },
   user_id: { type: String },
   quantity: { type: Number },
   total_cost: { type: Decimal128 },
 });
 
+const Cart = mongoose.model('Cart', cartSchema);
+
 // export model user with UserSchema
-module.exports = mongoose.model("cart", cartSchema);
+module.exports = Cart;
