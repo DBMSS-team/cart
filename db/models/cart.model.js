708191c85ae2d2cf8fe3ae21e258675ae5dd9a86
I355102 <sahil.kumar@sap.com>
2020-09-05 00:05:02 +0530

    cart model updated

--- db/models/cart.model.js
+++ db/models/cart.model.js
@@ -5,12 +5,12 @@ const Schema = mongoose.Schema;
 const cartProductSchema = new Schema({
   product_id: { type: String, required: true },
   quantity: { type: Number, required: true },
+  product_cost: { type: mongoose.Types.Decimal128, required: true }
 });
 
 const cartSchema = new Schema({
   products: [cartProductSchema],
   store_id: { type: String, required: true },
-  quantity: { type: Number, required: true },
   total_cost: { type: mongoose.Types.Decimal128, required: true },
 });
 
